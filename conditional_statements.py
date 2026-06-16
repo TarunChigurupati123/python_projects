f11f8b0713a9c0815e347262424c9685c622bd77
TarunChigurupati123 <youngruan31@gmail.com>
2026-06-16 14:20:53 +0530

    Add files via upload

---
+++ conditional_statements.py
@@ -0,0 +1,21 @@
+# studentGrade = int(input("Enter your marks: "))
+
+# pass_marks = 35
+
+# print(studentGrade >= pass_marks)
+
+# # if studentGrade >= pass_marks:
+# #     print("Pass")
+# # else:
+# #     print("Fail")
+
+score = 60
+
+if score >= 90:
+    print("Grade: A")
+elif score >= 80:
+    print("Grade: B")
+elif score >= 70:
+    print("Grade: C")
+else:
+    print("Grade: F")
---
+++ variables_demo.py
@@ -0,0 +1,19 @@
+# JOhn, 35000, Python developer, 35
+
+# name = "John"
+# salary = 35000
+# designation =  "Python developer"
+# age = 35
+
+name = input("Enter your name: ")
+salary = input("Enter your salary: ")
+designation = input("Enter your designation: ")
+age = input("Enter your age: ")
+
+
+# print(name, salary, designation, age)
+
+print(name)
+print(salary)
+print(designation)
+print(age)
